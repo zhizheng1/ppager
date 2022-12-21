c2ebe134f1b94773d35e490fc95f8a654aff85f0
zhizheng1 <114848324+zhizheng1@users.noreply.github.com>
2022-12-21 14:28:24 +0800

    Update ppager.sh

--- ppager.sh
+++ ppager.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-TTY=`tty`
-TEMPFILE=ppager_${TTY##*/}
+if [ -z "$1" ]; then
+	TTY=`tty`
+	TEMPFILE=ppager_${TTY##*/}
+else
+	TEMPFILE=$1
+fi
 
-echo '(PID=`cat /tmp/'$TEMPFILE'.pid`; rm -f /tmp/'$TEMPFILE'.pid; kill $PID; cat>/tmp/'$TEMPFILE'.fifo)' >/tmp/$TEMPFILE.sh
-chmod +x /tmp/$TEMPFILE.sh
+echo '(PID=`cat /tmp/'$TEMPFILE'.pid`; rm -f /tmp/'$TEMPFILE'.pid; kill $PID; cat>/tmp/'$TEMPFILE'.fifo)' >/tmp/$TEMPFILE
+chmod +x /tmp/$TEMPFILE
 
 mkfifo /tmp/$TEMPFILE.fifo
-echo "export PAGER=/tmp/$TEMPFILE.sh" >/tmp/$TEMPFILE.fifo &
+echo "export PAGER=/tmp/$TEMPFILE" >/tmp/$TEMPFILE.fifo &
 while true; do 
 	cat /tmp/$TEMPFILE.fifo|less -R &
 	PID=$!
@@ -16,4 +20,4 @@ while true; do
 	[ -f /tmp/$TEMPFILE.pid ] && break
 done
 
-rm -f /tmp/$TEMPFILE.sh /tmp/$TEMPFILE.fifo /tmp/$TEMPFILE.pid
+rm -f /tmp/$TEMPFILE /tmp/$TEMPFILE.fifo /tmp/$TEMPFILE.pid

7cb9114997e9c0b2e0377cd9fca9a2ab8aacf950
zhizheng1 <114848324+zhizheng1@users.noreply.github.com>
2022-12-22 18:31:52 +0800

    Update ppager.sh

--- ppager.sh
+++ ppager.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-	TTY=`tty`
-	TEMPFILE=/tmp/ppager_${TTY##*/}
+  TTY=`tty`
+  TEMPFILE=/tmp/ppager_${TTY##*/}
 else
-	TEMPFILE=/tmp/$1
+  TEMPFILE=/tmp/$1
 fi
 
-rm -f $TEMPFILE $TEMPFILE.fifo $TEMPFILE.pid
-echo 'PID=`cat '$TEMPFILE'.pid`; rm -f '$TEMPFILE'.pid; kill $PID; while [ ! -f '$TEMPFILE'.pid ]; do sleep 0.01; done; cat>'$TEMPFILE'.fifo; ec=$?; [ $ec -eq 141 ] && ec=0; exit $ec' >$TEMPFILE
+rm -f $TEMPFILE $TEMPFILE.fifo $TEMPFILE.pid $TEMPFILE.lock
+echo 'kill `cat '$TEMPFILE'.pid`; echo>'$TEMPFILE'.lock; cat>'$TEMPFILE'.fifo; EC=$?; [ $EC = 141 ] && EC=0; exit $EC' >$TEMPFILE
 chmod +x $TEMPFILE
 mkfifo $TEMPFILE.fifo
+mkfifo $TEMPFILE.lock
 START=1
 while true; do 
-	[ -n "$START" ] && echo "export PAGER=$TEMPFILE" >$TEMPFILE.fifo &
-	cat $TEMPFILE.fifo|less -R &
-	PID=$!
-	echo $PID >$TEMPFILE.pid
-	wait $PID
-	if [ -f $TEMPFILE.pid ]; then
-		[ -n "$START" ] && break
-		START=1
-	else
-		unset START
-	fi
+  [ -n "$START" ] && echo "export PAGER=$TEMPFILE" >$TEMPFILE.fifo &
+  cat $TEMPFILE.fifo|less -R &
+  PID=$!
+  echo $PID >$TEMPFILE.pid
+  [ -z "$START" ] && cat $TEMPFILE.lock >/dev/null
+  wait $PID
+  if [ $? = 15 ]; then
+    unset START
+  else
+    [ -n "$START" ] && break
+    START=1
+  fi
 done
-rm -f $TEMPFILE $TEMPFILE.fifo $TEMPFILE.pid
+rm -f $TEMPFILE $TEMPFILE.fifo $TEMPFILE.pid $TEMPFILE.lock
